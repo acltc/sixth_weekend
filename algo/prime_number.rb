cb69b34f020ae9f331b156e7c996edd196a71fc3
Jay Wengrow <jaywngrw@gmail.com>
2016-02-11 07:23:54 -0600

    initial commit

---
+++ algo/prime_number.rb
@@ -0,0 +1,17 @@
+# Use RSpec to test the following exercise! You may wish to wrap the methods below in a class to help facilitate this.
+
+# First, implement the is_prime? method below. It should accept a number as an argument and return
+# true if the number is prime and false if it is not. A prime number is a number that is only
+# divisible by itself and the number 1.
+
+def is_prime?(number)
+
+end
+
+# Second, implement the highest_prime_number_under method below. It should accept a number as
+# an argument and return the highest prime number under that number. For example, the 
+# highest prime number under 10 is 7.
+
+def highest_prime_number_under(number)
+
+end
---
+++ oop/thesaurus.rb
@@ -0,0 +1,14 @@
+# Create a thesaurus application. To create it, you will create two classes, Thesaurus and Entry.
+# A Thesaurus will contain many Entries. An Entry contains three primary attributes:
+# word, synonymns, and antonyms. Your application should contain the following features:
+# 1. The ability to add new words to a Thesaurus.
+# 2. The ability to delete a word from a Thesaurus. 
+# 3. The ability to look up a word's synonyms.
+# 4. The ability to look up a word's antonyms.
+# 5. The ability to add a synonym to a word.
+# 6. The ability to add an antonym to a word.
+#
+# Part of the challenge is to determine which functionality belongs in the Thesaurus class,
+# and which belongs in the Entry class.
+#
+# And... test your functionality using RSpec!
